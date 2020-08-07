4a8fb8819f6bfb3bece294f7110baef1163fa5d6
douknowme0852 <69332250+douknowme0852@users.noreply.github.com>
2020-08-07 11:13:32 +0700

    Update Exercise2.c

--- Exercise2.c
+++ Exercise2.c
@@ -11,7 +11,23 @@ Ex:
 #include <stdlib.h>
 
 int ex2(int n, char *str[]){
-	//your codes here
+	char name[25][50],temp[25];
+  int n,i,j;
+	for(i=0;i<=n;i++)
+  {
+       
+       fgets(name[i], sizeof name, stdin);
+  }
+    for(i=1;i<=n;i++)
+	for(j=0;j<=n-i;j++)
+	if(strcmp(name[j],name[j+1])>0)
+	{   
+		strcpy(temp,name[j]);
+		strcpy(name[j],name[j+1]);
+	    strcpy(name[j+1],temp);
+	}
+	for(i=0;i<=n;i++){
+		printf("%s\n",name[i]);}
 }
 
 int main(int argc, char *argv[]) {
